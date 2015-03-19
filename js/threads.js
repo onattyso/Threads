e064ac71d0716ff7dceebe281a221a7423af0c94
Natalie Sun <onattyso@gmail.com>
2015-03-18 22:25:04 -0700

    Moving things around
    Moved THREADS function below all of the scene starters

--- js/threads.js
+++ js/threads.js
@@ -1,67 +1,5 @@
 //code from index.html
 
-var THREADS = (function() {
-  return {
-    aBunchOfCubes: [],
-    init: function() {
-      console.log("Initializing Threads!");
-
-      console.log("Creating some cubes...");
-      var geometry = new THREE.BoxGeometry(15,15,15);// = Some geometry here;
-
-      for ( i = 0; i < 600; i++ ) {
-
-	      var cube = new THREE.Mesh(geometry, (new THREE.MeshPhongMaterial({color: Math.random() * 0xffffff})));
-	      		cube.position.x = Math.random() * 800 - 400;
-				cube.position.y = Math.random() * 800 - 400;
-				cube.position.z = Math.random() * 800 - 400;
-
-				cube.rotation.x = Math.random() * 2 * Math.PI;
-				cube.rotation.y = Math.random() * 2 * Math.PI;
-				cube.rotation.z = Math.random() * 2 * Math.PI;
-
-	      scene.add(cube);
-	  }
-
-
-      this.createCube();
-
-      console.log("Listing all of the cubes we just made...");
-      this.listCubes();
-      //write a function that draws to the screen
-    },
-    createCube: function() {
-      console.log("Making a 3D cube!");
-      // Build a cube here...      // var material = new THREE.MeshPhongMaterial({color: Math.random() * 0xffffff});// = Some material here;
-      
-      // And then save it to the aBunchOfCubes array:
-      this.aBunchOfCubes.push(cube);
-
-      // Comment this out to STOP adding FAKE cubes to the list:
-      this.aBunchOfCubes.push("NOT A CUBE! Go write some code!");
-    },
-    listCubes: function() {
-      var cubeCount = this.aBunchOfCubes.length;
-      if (cubeCount == 0) {
-        console.log("No cubes found!");
-      } else {
-        console.log(cubeCount + " cubes found!");
-        for (var i = 0; i < cubeCount; i++) {
-          console.log("Cube " + i + ":");
-          console.dir(this.aBunchOfCubes[i]);
-        }
-      }
-
-    }
-  };
-})();
-
-
-
-
-
-
-
 
 //Setup three.js WebGL renderer
 var renderer = new THREE.WebGLRenderer({ antialias: true });
@@ -105,6 +43,66 @@ line.rotation.x = -50*Math.PI/180;
 
 // Add mesh to your three.js scene
 scene.add(line);
+
+var THREADS = (function() {
+  return {
+    aBunchOfCubes: [],
+    init: function() {
+      console.log("Initializing Threads!");
+
+      console.log("Creating some cubes...");
+
+      var geometry = new THREE.BoxGeometry(15,15,15);// = Some geometry here;
+      var material = new THREE.MeshPhongMaterial({color: Math.random() * 0xffffff});
+	  var cube = new THREE.Mesh(geometry, material);
+
+      for ( i = 0; i < 600; i++ ) {
+
+      			cube.material = new THREE.MeshPhongMaterial({color: Math.random() * 0xffffff});
+	      // var cube = new THREE.Mesh(geometry, (new THREE.MeshPhongMaterial({color: Math.random() * 0xffffff})));
+	      		cube.position.x = Math.random() * 800 - 400;
+				cube.position.y = Math.random() * 800 - 400;
+				cube.position.z = Math.random() * 800 - 400;
+
+				cube.rotation.x = Math.random() * 2 * Math.PI;
+				cube.rotation.y = Math.random() * 2 * Math.PI;
+				cube.rotation.z = Math.random() * 2 * Math.PI;
+
+	      scene.add(cube);
+	  }
+
+      this.createCube();
+
+      console.log("Listing all of the cubes we just made...");
+      this.listCubes();
+      //write a function that draws to the screen
+    },
+    createCube: function() {
+      console.log("Making a 3D cube!");
+      // Build a cube here...
+      
+      // And then save it to the aBunchOfCubes array:
+      this.aBunchOfCubes.push(cube);
+
+      // Comment this out to STOP adding FAKE cubes to the list:
+      this.aBunchOfCubes.push("NOT A CUBE! Go write some code!");
+    },
+    listCubes: function() {
+      var cubeCount = this.aBunchOfCubes.length;
+      if (cubeCount == 0) {
+        console.log("No cubes found!");
+      } else {
+        console.log(cubeCount + " cubes found!");
+        for (var i = 0; i < cubeCount; i++) {
+          console.log("Cube " + i + ":");
+          console.dir(this.aBunchOfCubes[i]);
+        }
+      }
+
+    }
+  };
+})();
+
 
 // Request animation frame loop function
 function animate() {
@@ -170,88 +168,3 @@ $(document).ready(function() {
 
 
 
-
-//Our animation
-// var lines = [];
-
-
-// var THREADS = (function() {
-// 	return {
-// 		line1: null,
-// 		line2: null,
-// 		line3: null
-// 		init: function() {
-// 			this.line1 = new OurVRLine();
-// 			this.line2 = new OurVRLine();
-// 			this.line3 = new OurVRLine();
-// 		},
-// 		load: function() {
-
-// 		},
-// 		draw: function() {
-// 			this.line1.draw();
-// 			this.line2.draw();
-// 			this.line3.draw();
-// 		},
-// 		update: function() {
-
-// 		},
-// 		resize: function() {
-
-// 		}
-// 	};
-// })();
-
-
-
-
-
-
-// function OurVRLine() {
-//   Console.log("Initalizing the user input handler...");
-//   this.init();
-//   Console.log("User Input initialized!")
-// }
-
-// OurVRLine.prototype = {
-//   this.x = 0;
-//   this.y = 0;
-//   this.z = 0;
-
-
-
-//   // Do we need this? Since we won't be having any interaction with the lines.//
-//   // pressed: {},
-//   // LEFT: 37,
-//   // UP: 38,
-//   // RIGHT: 39,
-//   // DOWN: 40,
-//   // SPACEBAR: 32, 
-//   // init: function() {
-//   //   Console.log("Keyboard init...");
-//   //   var that = this;
-//   //   window.addEventListener('keyup', function(event) { that.onKeyup(event); }, false);
-//   //   window.addEventListener('keydown', function(event) { that.onKeydown(event); }, false);
-//   // },
-//   // isDown: function(keyCode) {
-//   //   return this.pressed[keyCode];
-//   // },
-//   // onKeydown: function(event) {
-//   //   // console.log("keyCode pressed: " + event.keyCode);
-//   //   this.pressed[event.keyCode] = true;
-//   // },
-//   // onKeyup: function(event) {
-//   //   delete this.pressed[event.keyCode];
-//   // },
-//   draw: function() {
-// //  	actually draws shit ONLY FOR THIS ONE INSTANCE OF ANY GIVEN LINE.
-//   }
-// };
-
-
-
-
-
-// $(document).ready(function() {
-// 	THREADS.init();
-// });
